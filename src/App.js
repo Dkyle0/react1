1659ec2df6cc16be14eaac040a696eeca5c64707
Dkyle0 <50553897+Dkyle0@users.noreply.github.com>
2024-01-20 14:25:13 +0300

    init project on react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const currentYear = new Date().getFullYear(); //  императивный
 
-export default App;
+export const App = () => {
+	// ниже, вроде бы, всё декларативный
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				<div className="time">{currentYear}</div>
+			</header>
+		</div>
+	);
+};
