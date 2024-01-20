bcf79756cfd07b4c8756939106ece27523ad02a7
Dkyle0 <50553897+Dkyle0@users.noreply.github.com>
2024-01-20 21:57:35 +0300

    React.createElement

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-export const App = () => {
-	const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="time">{currentYear}</div>
-			</header>
-		</div>
+export const App = () => {
+	return React.createElement(
+		'div',
+		{ className: 'App' },
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			React.createElement(
+				'p',
+				null,
+				'Edit ',
+				React.createElement('code', null, 'src/App.js'),
+				' and save to reload.',
+			),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement('div', { className: 'time' }, currentYear),
+		),
 	);
 };
