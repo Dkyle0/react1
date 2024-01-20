d125bbdf5da30fddf3746f6dfd9a7e51f78d7cdd
Dkyle0 <50553897+Dkyle0@users.noreply.github.com>
2024-01-20 21:54:59 +0300

    rename variables

--- src/App.js
+++ src/App.js
@@ -1,54 +1,54 @@
 import styles from './App.module.css';
 import { useState } from 'react';
-let operatorUsed = ''; // нормально декларировать тут эти переменные? Можно было это как-то, как-то лучше сделать?
-let firstNbr = '';
+let operand1 = ''; // нормально декларировать тут эти переменные? Можно было это как-то, как-то лучше сделать?
+let operand2 = '';
 
 export const App = () => {
 	let [currentDisplay, setCurrentDisplay] = useState('0');
 	const operators = ['+', '-'];
-	const numbersKeys = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
+	const NUMS = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
 	const addNbrToCalc = (event) => {
 		currentDisplay === '0'
 			? setCurrentDisplay(event.target.textContent)
 			: setCurrentDisplay(currentDisplay + event.target.textContent);
 	};
 	const addOperarorToCalc = (event) => {
-		if (!operatorUsed) {
-			firstNbr = currentDisplay;
+		if (!operand1) {
+			operand2 = currentDisplay;
 			setCurrentDisplay('0');
-			operatorUsed = event.target.textContent;
+			operand1 = event.target.textContent;
 		}
 	};
 	const acBtn = () => {
-		operatorUsed = '';
+		operand1 = '';
 		setCurrentDisplay('0');
 	};
 	const calcResult = () => {
-		console.log(operatorUsed, firstNbr);
-		if (operatorUsed.length < 1 || firstNbr.length < 1) {
-			operatorUsed = '';
-			firstNbr = '';
+		console.log(operand1, operand2);
+		if (operand1.length < 1 || operand2.length < 1) {
+			operand1 = '';
+			operand2 = '';
 			setCurrentDisplay('0');
 			alert('Ошибка');
 		}
-		if (operatorUsed === '+') {
-			const result = Number(firstNbr) + Number(currentDisplay);
+		if (operand1 === '+') {
+			const result = Number(operand2) + Number(currentDisplay);
 			setCurrentDisplay(result.toString());
-			operatorUsed = '';
-			firstNbr = '';
+			operand1 = '';
+			operand2 = '';
 		}
-		if (operatorUsed === '-') {
-			const result = Number(firstNbr) - Number(currentDisplay);
+		if (operand1 === '-') {
+			const result = Number(operand2) - Number(currentDisplay);
 			setCurrentDisplay(result.toString());
-			operatorUsed = '';
-			firstNbr = '';
+			operand1 = '';
+			operand2 = '';
 		}
 	};
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div className={firstNbr.length > 0 ? styles.calculatorOutput : null}>
-					{firstNbr}
+				<div className={operand2.length > 0 ? styles.calculatorOutput : null}>
+					{operand2}
 				</div>
 				<div className={styles.calculator}>
 					<div className={styles.calculatorOutput}>{currentDisplay}</div>
@@ -64,7 +64,7 @@ export const App = () => {
 								</button>
 							);
 						})}
-						{numbersKeys.map((key) => {
+						{NUMS.map((key) => {
 							return (
 								<button
 									key={key}
