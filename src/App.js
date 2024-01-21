6d3e14084ddf8cbf69af5bfc94a661513dd143d5
Dkyle0 <50553897+Dkyle0@users.noreply.github.com>
2024-01-21 12:48:25 +0300

    remade to meet all the conditions of the task

--- src/App.js
+++ src/App.js
@@ -1,63 +1,82 @@
 import styles from './App.module.css';
 import { useState } from 'react';
 let operand1 = ''; // нормально декларировать тут эти переменные? Можно было это как-то, как-то лучше сделать?
+let operator = '';
 let operand2 = '';
+
+const resetAll = () => {
+	operand1 = '';
+	operator = '';
+	operand2 = '';
+};
 
 export const App = () => {
 	let [currentDisplay, setCurrentDisplay] = useState('0');
+	const [isResult, setIsResult] = useState(false);
 	const operators = ['+', '-'];
 	const NUMS = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0'];
 	const addNbrToCalc = (event) => {
 		currentDisplay === '0'
 			? setCurrentDisplay(event.target.textContent)
 			: setCurrentDisplay(currentDisplay + event.target.textContent);
+		if (isResult) {
+			setIsResult(false);
+		}
 	};
 	const addOperarorToCalc = (event) => {
-		if (!operand1) {
-			operand2 = currentDisplay;
-			setCurrentDisplay('0');
-			operand1 = event.target.textContent;
+		if (operator.length < 1) {
+			setIsResult(false);
+			operator = event.target.textContent;
+			operand1 = currentDisplay;
+			setCurrentDisplay(`${currentDisplay}  ${operator} `);
 		}
 	};
 	const acBtn = () => {
-		operand1 = '';
+		resetAll();
+		setIsResult(false);
 		setCurrentDisplay('0');
 	};
 	const calcResult = () => {
-		console.log(operand1, operand2);
-		if (operand1.length < 1 || operand2.length < 1) {
-			operand1 = '';
-			operand2 = '';
+		const tmp = currentDisplay.split(' ');
+		operand2 = tmp[3];
+		if (operand1.length < 1 || operand2.length < 1 || operator.length < 1) {
+			resetAll();
 			setCurrentDisplay('0');
+			setIsResult(false);
 			alert('Ошибка');
 		}
-		if (operand1 === '+') {
-			const result = Number(operand2) + Number(currentDisplay);
+		if (operator === '+') {
+			const result = Number(operand1) + Number(operand2);
 			setCurrentDisplay(result.toString());
-			operand1 = '';
-			operand2 = '';
+			resetAll();
+			setIsResult(true);
 		}
-		if (operand1 === '-') {
-			const result = Number(operand2) - Number(currentDisplay);
+		if (operator === '-') {
+			const result = Number(operand1) - Number(operand2);
 			setCurrentDisplay(result.toString());
-			operand1 = '';
-			operand2 = '';
+			resetAll();
+			setIsResult(true);
 		}
 	};
 	return (
 		<div className="App">
 			<header className="App-header">
-				<div className={operand2.length > 0 ? styles.calculatorOutput : null}>
-					{operand2}
-				</div>
 				<div className={styles.calculator}>
-					<div className={styles.calculatorOutput}>{currentDisplay}</div>
+					<div
+						className={
+							isResult
+								? `${styles.calculatorOutput} ${styles.result}`
+								: styles.calculatorOutput
+						}
+					>
+						{currentDisplay}
+					</div>
 					<div className={styles.calculatorKeys}>
 						{operators.map((operator) => {
 							return (
 								<button
 									key={operator}
-									className={`${styles.calculatorKey} ${styles.calculatorKeyOperator}`}
+									className={`${styles.calculatorKey} ${styles.calculatorKeyOperator}`} // для 2 части задания
 									onClick={addOperarorToCalc}
 								>
 									{operator}
@@ -76,7 +95,7 @@ export const App = () => {
 							);
 						})}
 						<button className={styles.calculatorAc} onClick={acBtn}>
-							AC
+							C
 						</button>
 						<button
 							className={styles.calculatorKeyEnter}
